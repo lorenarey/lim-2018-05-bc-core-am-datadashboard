72ea90ce82b5f7f88d991fbd50a7fcdb6eef6183
lorenareyes <lorenacrisreyes@gmail.com>
2018-06-30 20:17:01 -0500

    subiendo funcion

--- src/data.js
+++ src/data.js
@@ -1,20 +1,98 @@
-window.computeUsersStats = () => {
-  const responseContainerElem = document.getElementById('response-container')
-  const userWithStats = cohortData.users.map(function (obj) {
+window.computeUsersStats = (users, progress, courses) => {
+  /*const responseContainerElem = document.getElementById('response-container')*/
+  const getResult = users.map(function (user) {
+    courses.forEach(courseName => {
+      if(progress[user.id].hasOwnProperty(courseName)){
+        const percent = progress[user.id].intro.percent;
+        const userUnits = progress[user.id].intro.units;
+        //obteniendo ejercicios//
+        let totalExcercises = 0;
+        let completedExercises = 0;
+        let totalReads = 0;
+        let completedReads= 0;
+        let totalQuizzes=0;
+        let completedQuizzes=0;
+        let scoreSum=0;
+        let scoreAvg=0;
+        Object.keys(userUnits).forEach((unitName) => {
+          const parts = userUnits[unitName].parts
+          Object.keys(parts).forEach((partName) => {
+            const part = parts[partName];
+            if (part.hasOwnProperty('exercises')) {
+              const exercises = part.exercises;
+              Object.keys(exercises).forEach((exerciseName) => {
+                const excercise = exercises[exerciseName]
+                totalExcercises += 1;
+                completedExercises += excercise.completed;
+              })
+            }
+            if (part.hasOwnProperty('type')) {
+              if(part.type === 'read'){
+              totalReads += 1; 
+              completedReads += part.completed 
+              }
+            }       
+            if (part.hasOwnProperty('type')) {
+              if(part.type === 'quiz'){
+              totalQuizzes +=1;
+              completedQuizzes += part.completed;
+              scoreSum += part.score ? part.score : 0; //si existe 'score' sumarle el valor del mismo, si no exite, no sumarle nada y continuar
+              scoreAvg = scoreSum/completedQuizzes ? scoreSum/completedQuizzes : 0;
+              }
+            }    
+          })
+        })
+        const percentExercises = (completedExercises / totalExcercises)*100
+        
+        /*console.log(user.id, 'Percent completed exercises', percentExercises)
+        console.log(totalExcercises);
+        console.log(completedExercises);*/
 
-    if(cohortData.progress[obj.id].hasOwnProperty('intro')){
-      const completedExercises = (cohortData.progress[obj.id]['intro']['units']['02-variables-and-data-types']['parts']['06-exercises']['completed']) * 100;
-      const userResult = { name: obj.name, stats: {exercises: completedExercises} }
-      const liElem = document.createElement('li')
-      liElem.innerHTML = userResult.name + '<b> ejercicios completados: ' + userResult.stats.exercises + '%</b>';
-      responseContainerElem.appendChild(liElem)
-      return userResult;
-    }
+        const percentReads = (completedReads / totalReads)*100
 
+        /*console.log(user.id, 'Percent completed Reads', percentReads)
+        console.log(totalReads);
+        console.log(completedReads);*/
+
+        const percentQuizzes = (completedQuizzes / totalQuizzes)*100
+        
+        console.log(percent)
+        console.log(user.id, 'Percent completed Quizzes', percentQuizzes.toFixed(2))
+        console.log(totalQuizzes);
+        console.log(completedQuizzes)
+        console.log(scoreSum)
+        console.log(scoreAvg.toFixed(2))
+
+
+        //--------------------------------------------------------//
+        
+
+        /*const userWithStats = {
+          name: user.name,
+          stats: {
+            percent:
+            exercises: {
+              total:
+              completed:
+              percent:
+            }
+            reads: {
+              total:
+              completed:
+              percent:
+            }
+            quizzes: {
+              total:
+              completed:
+              percent:
+              scoreSum:
+              scoreAvg:
+            }
+          }
+        }*/
+      } 
+      return null;
+    })
   });
-  console.log(computeUsersStats);
-
  /*return userWithStats;*/
 }
-
-
--- src/main.js
+++ src/main.js
@@ -1,22 +1,60 @@
 const names = document.getElementById('cohort-lima')
 const responseContainer = document.getElementById('response-container')
+const usersUrl = '../data/cohorts/lim-2018-03-pre-core-pw/users.json'
+const progressUrl = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json'
+const cohortsUrl = '../data/cohorts.json'
+let users 
+let progress
+let courses
+
+//declaramos el objeto global cohort data vacío que se llenará cuando se obtenga la data
+// las variables se puede cargar de cualquier archivo por que esta dentro del ob windows //
+
+const saveUsers = (event) => {
+  //guardndo la data user en cohortData.users
+  users = JSON.parse(event.target.responseText);
+  getData(progressUrl, saveProgress, 'progress');
+
+}
+
+const saveProgress = (event) => {
+  progress = JSON.parse(event.target.responseText);
+  getData(cohortsUrl, saveCohorts, 'cohorts');
+
+}
+
+const saveCohorts = (event) => {
+  cohorts = JSON.parse(event.target.responseText);
+  const currentCohort = cohorts.find((cohort) => {
+    return cohort.id === 'lim-2018-03-pre-core-pw';
+  });
+
+  courses = Object.keys(currentCohort.coursesIndex)
+
+computeUsersStats(users, progress, courses)
+
+}
+
+const handleError = () => {
+  console.log('hay un error')
+}
+
+//Creamos una funcion donde pasamos de parametro las url y el onload
+const getData = (url, callback, stringData) => {
+
+  let requestData = new XMLHttpRequest();
+  requestData.open('GET', url);
+  requestData.onload = callback;
+  requestData.onerror = handleError;
+  requestData.send();
+};
+
+getData(usersUrl, saveUsers, 'users');
 
 names.addEventListener('click', (e) => {
   e.preventDefault();
 
-  computeUsersStats()
 
-
-  /*let liobj =document.createElement('li');
-      liobj.innerHTML = newUserWithStats;
-      responseContainer.appendChild(liobj);
-   //hacer un for para imprimir  el listado */
-
-  //let printnewUserWiS = for (let printUserWiS of UserWithStats) {
-    //return
-//
-
-  //responseContainer.appendChild(li);
 });
 
 
