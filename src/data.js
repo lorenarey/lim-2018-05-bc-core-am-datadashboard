dd22bdd9b462f7746c64725ee345ec6d432c5d93
lorenareyes <lorenacrisreyes@gmail.com>
2018-07-01 19:26:05 -0500

    subiendo funciones filter y sort

--- src/data.js
+++ src/data.js
@@ -1,19 +1,20 @@
 window.computeUsersStats = (users, progress, courses) => {
-  const getResult = users.map(function (user) {
+  const usersWithStats = users.map((user) => {
+    let percent;
+    let totalExcercises = 0;
+    let completedExercises = 0;
+    let totalReads = 0;
+    let completedReads= 0;
+    let totalQuizzes=0;
+    let completedQuizzes=0;
+    let scoreSum=0;
+    let scoreAvg=0;
+
     courses.forEach(courseName => {
-      console.log('courseName', courseName)
       if(progress[user.id].hasOwnProperty(courseName)){
-        const percent = progress[user.id].intro.percent;
+        percent = progress[user.id].intro.percent;
         const userUnits = progress[user.id].intro.units;
-        //obteniendo ejercicios//
-        let totalExcercises = 0;
-        let completedExercises = 0;
-        let totalReads = 0;
-        let completedReads= 0;
-        let totalQuizzes=0;
-        let completedQuizzes=0;
-        let scoreSum=0;
-        let scoreAvg=0;
+        
         Object.keys(userUnits).forEach((unitName) => {
           const parts = userUnits[unitName].parts
           Object.keys(parts).forEach((partName) => {
@@ -21,7 +22,7 @@ window.computeUsersStats = (users, progress, courses) => {
             if (part.hasOwnProperty('exercises')) {
               const exercises = part.exercises;
               Object.keys(exercises).forEach((exerciseName) => {
-                const excercise = exercises[exerciseName]
+                const excercise = exercises[exerciseName];
                 totalExcercises += 1;
                 completedExercises += excercise.completed;
               })
@@ -42,60 +43,99 @@ window.computeUsersStats = (users, progress, courses) => {
             }    
           })
         })
-        const percentExercises = (completedExercises / totalExcercises)*100
-        
-        /*console.log(user.id, 'Percent completed exercises', percentExercises)
-        console.log(totalExcercises);
-        console.log(completedExercises);*/
 
-        const percentReads = (completedReads / totalReads)*100
-
-        /*console.log(user.id, 'Percent completed Reads', percentReads)
-        console.log(totalReads);
-        console.log(completedReads);*/
-
-        const percentQuizzes = (completedQuizzes / totalQuizzes)*100
-        
-        console.log(percent)
-        console.log(user.id, 'Percent completed Quizzes', percentQuizzes.toFixed(2))
-        console.log(totalQuizzes);
-        console.log(completedQuizzes)
-        console.log(scoreSum)
-        console.log(scoreAvg.toFixed(2))
-
-
-        //--------------------------------------------------------//
-        
-
-        const usersWithStats = {
-          name: user.name,
-          stats: {
-            percent: percent,
-            exercises: {
-              total: totalExcercises,
-              completed: completedExercises,
-              percent: percentExercises,
-            },
-            reads: {
-              total: totalReads,
-              completed: completedReads,
-              percent: percentReads,
-            },
-            quizzes: {
-              total: totalQuizzes,
-              completed: completedQuizzes,
-              percent: percentQuizzes,
-              scoreSum: scoreSum,
-              scoreAvg: scoreAvg,
-            }
-          }
-        }
-        
-        console.log(usersWithStats)
-
-        return usersWithStats;
       } 
     })
+
+    const percentExercises = (completedExercises / totalExcercises)*100
+    const percentReads = (completedReads / totalReads)*100
+    const percentQuizzes = (completedQuizzes / totalQuizzes)*100
+
+    const newUser = {
+      name: user.name,
+      stats: {
+        percent: Math.round(percent),
+        exercises: {
+          total: totalExcercises,
+          completed: completedExercises,
+          percent: percentExercises,
+        },
+        reads: {
+          total: totalReads,
+          completed: completedReads,
+          percent: Math.round(percentReads),
+        },
+        quizzes: {
+          total: totalQuizzes,
+          completed: completedQuizzes,
+          percent: Math.round(percentQuizzes),
+          scoreSum: Math.round(scoreSum),
+          scoreAvg: Math.round(scoreAvg),
+        }
+      }
+    }
+    
+    return newUser
   });
- return getResult;
+  return usersWithStats;
+  
+}
+
+window.sortUsers = (users, orderBy, OrderDirection) => {
+  let sortingList = users;
+  if (orderBy === "Nombre") {
+    sortingList.sort((a, b) => {
+      let firstUser = a.stats.name.toLowerCase(), lastUser = b.stats.name.toLowerCase();
+      if (fisrtUser < lastUser)
+        return -1
+      if (firstUser > lastUser)
+        return 1
+    });
+  }
+  if (orderBy === "% de Completitud Total") {
+    sortingList.sort((a, b) => {
+      return a.stats.percent - b.stats.percent;
+    });
+  }
+  if (orderBy === "% de Ejercicios completados") {
+    sortingList.sort((a, b) => {
+      return a.stats.exercises.completed - b.stats.exercises.completed;
+    });
+  }
+  if (orderBy === "% de Lecturas completadas") {
+    sortingList.sort((a, b) => {
+    return a.stats.reads.completed - b.stats.reads.completed;  
+    });
+  }
+  if (orderBy === "% de Quizzes completadas") {
+    sortingList.sort((a, b) => {
+      return a.stats.quizzes.completed - b.stats.quizzes.completed;
+    });
+  }  
+  if (orderBy === "Promedio de Quizzes") {
+    sortingList.sort((a, b) => {
+      return a.stats.quizzes.scoreAvg - b.stats.quizzes.scoreAvg;
+    });
+  }
+  if (OrderDirection === "desc"){
+    sorting = sortingList.reverse();
+  }
+  console.log(sortingList)
+  return sortingList;
+}
+
+window.filterUsers = (users, search) => {
+  let filteringList = users.filter(user => (user.name.toUpperCase()).indexOf(search.toUpperCase()) !== -1);
+  return filteringList;
+}
+
+window.processCohortData = (options) => {
+  // cohort: [{},{}],
+  // cohortData: {
+  //   users: [{},{}],
+  //   progress: {},
+  // },
+  // orderBy:,
+  // orderDirection:,
+  // search:,
 }
--- test/data.spec.js
+++ test/data.spec.js
@@ -58,6 +58,7 @@ describe('data', () => {
           total: 3,
           completed: 2,
           percent: 67,
+          scoreSum: 57,
           scoreAvg: 29,
         });
       });
